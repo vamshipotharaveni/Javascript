7ae20ed538ad4064d2d6643503fad213e5164a2a
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2024-08-24 14:09:15 +0530

    updated

---
+++ index.js
@@ -0,0 +1,88 @@
+// function myfun(s){
+//     s=s.toLowerCase();
+//     vwl="aeiou"
+//     count=0
+//     for(const char of s){
+//         if (vwl.includes(char)){
+//             count++;
+//         }
+//     }
+//     console.log(count)
+// }
+// myfun("apna collage")
+
+
+
+// let a=(s)=>{
+//     s=s.toLowerCase();
+//     vwl="aeiou"
+//     count=0
+//     for(const char of s){
+//         if (vwl.includes(char)){
+//             count++;
+//         }
+//     }
+//     console.log(count)
+// }
+
+
+
+
+// arr.forEach((val,ind) => {
+//     console.log(val**2)
+// });
+
+
+
+
+// let arr2=arr.map((val)=>{
+//     return val*2;
+
+// })
+// console.log(arr2);
+
+
+
+// let vals=arr.filter((val)=>{
+//     if (val%2===0){
+//         return val;
+//     }
+// })
+// console.log(vals);
+
+
+// let a=arr.reduce((res,val)=>{
+   
+//         if (res<val){
+//             res=val;
+//         }
+//     return res
+// },arr[0]);
+// console.log(a)
+
+
+// let arr=[94,75,67,97,86];
+
+// let a=arr.filter((val)=>{
+//     if (val>90){
+//         return val
+//     }
+    
+// });
+// console.log(a);
+
+
+// let a=parseInt(prompt("enter a number "));
+// let arr=[]
+// for(let i=1;i<=a;i++){
+//     arr.push(i)
+// }
+
+
+// let x=arr.reduce((res,cur)=>{
+//     return res=res*cur
+// })
+// console.log(x);
+
+let a=document.querySelector(".first-div")
+console.log(a);
